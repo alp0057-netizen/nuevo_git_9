3a7a2c77dcf5d40adff6eb34ebca25688f7e791d
antonio lizana <alp0057@alu.medac.es>
2026-03-18 11:28:00 +0100

    Comandos de GIT

--- Proyecto Entornos/src/App.java
+++ Proyecto Entornos/src/App.java
@@ -4,5 +4,20 @@ public class App {
         System.out.println();
         System.out.println("Antonio Lizana Palomnio");
         System.out.println("Saúl Garcia Navas");
+        System.out.println();
+        System.out.println("Comandos de GIT: ");
+        System.out.println("git init");
+        System.out.println("git add");
+        System.out.println("git commit");
+        System.out.println("git push");
+        System.out.println("git pull");
+        System.out.println("git clone");
+        System.out.println("git status");
+        System.out.println("git branch");
+        System.out.println("git merge");
+        System.out.println("git log");
+        System.out.println("git checkout");
+
+        
     }
 }
